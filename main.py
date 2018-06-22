0dd84f9ff49a5cc0684793b9a55d24b0b214c36e
Andrei Tiurin <at@braingarden.ai>
2018-06-22 16:37:20 +0300

    rename

--- main.py
+++ main.py
@@ -1,43 +1,19 @@
 import cv2
 from Region import Region
+import numpy as np
+import GraphCoarse
 
+import utils
 
-img = cv2.imread('test.jpg',0)
+if __name__ == '__main__':
 
-pixel_regions = []
+    image = utils.getImage('datasets/rsz_test_keyboard.jpg')
 
-# print(img.shape)
-id = 0
-for r in range(img.shape[0]):
-    row = []
-    for c in range(img.shape[1]):
-        reg = Region(True,img[r,c])
-        id+=1
-        row.append(reg)
-    pixel_regions.append(row)
+    G0 = GraphCoarse.image_to_graph(image)
+    Gs = GraphCoarse.coarse_0(G0)
 
-for r in range(img.shape[0]):
-    for c in range(img.shape[1]):
-        pixel = pixel_regions[r][c]
-        if r != 0:
-            nb = pixel_regions[r-1][c]
-            pixel.addNeighbor(nb,0)
-        if r != img.shape[0]-1:
-            nb = pixel_regions[r+1][c]
-            pixel.addNeighbor(nb, 2)
-        if c != 0:
-            nb = pixel_regions[r][c-1]
-            pixel.addNeighbor(nb, 3)
-        if c != img.shape[1]-1:
-            nb = pixel_regions[r-1][c]
-            pixel.addNeighbor(nb, 1)
+    while len(Gs)>=5:
+        Gs = GraphCoarse.coarse(Gs)
+        print("Current:",len(Gs))
 
-for r in range(img.shape[0]):
-    for c in range(img.shape[1]):
-        pixel = pixel_regions[r][c]
-        pixel.calc_weights()
-
-cv2.imshow('image',img)
-k = cv2.waitKey(1000)
-
-
+    utils.debugImagePixels(Gs,image)
